0b03ae5f49d41cc6074726c7825ddc679c9ec615
Thar Htoo Sat <tharhtoosatt@gmail.com>
2025-11-20 11:18:44 +0630

    JS ToDo List

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a+b; 
+}
+
+function sub(a ,b) {
+    if (condition){
+        return a -b ;
+    }else {
+        return b - b;
+    }
+}
+
+// function show(){
+
+// }
+// function hide(){
+
+// }
