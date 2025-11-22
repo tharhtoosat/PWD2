dcdeff5ba8983f57845ce44f22541e87716f057b
Thar Htoo Sat <tharhtoosatt@gmail.com>
2025-11-22 10:58:04 +0630

    JS Module

---
+++ a.js
@@ -0,0 +1,23 @@
+// Common Module
+/*
+const PI = 3.14;
+
+function add(a, b){
+    return a+b;
+}
+function sub(a, b){
+    return a-b;
+}
+
+module.exports = { PI, add };
+*/
+//ES Module
+export const PI = 3.14;
+
+export function add(a, b){
+    return a+b;
+}
+function sub(a, b){
+    return a-b;
+}
+
---
+++ b.js
@@ -0,0 +1,12 @@
+/* Common Module
+const { PI, add } = require("./a.js");
+
+console.log(PI);
+console.log(add(1, 2));
+*/
+
+// ES Module
+import { PI, add }  from "./a.js";
+
+console.log(PI);
+console.log(add(1, 2));
