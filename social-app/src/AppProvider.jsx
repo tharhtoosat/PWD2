6a6474c836387519dfcd812f4602cd05115a6027
Thar Htoo Sat <tharhtoosatt@gmail.com>
2025-12-31 12:00:26 +0630

    Completed APP

--- social-app/src/AppProvider.jsx
+++ social-app/src/AppProvider.jsx
@@ -1,6 +1,6 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { createContext, useContext, useMemo, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 
 const AppContext = createContext();
 export function useApp() {
@@ -11,6 +11,25 @@ export default function AppProvider({ children }) {
   const [mode, setMode] = useState("dark");
   const [openDrawer, setOpenDrawer] = useState(false);
   const [auth, setAuth] = useState();
+
+  useEffect(() => {
+    const api = "http://localhost:8800";
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch(`${api}/users/verify`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then(async (res) => {
+        if (res.ok) {
+          const user = await res.json();
+          setAuth(user);
+        } else {
+          localStorage.removeItem("token");
+        }
+      });
+    }
+  }, []);
   const theme = useMemo(() => {
     return createTheme({
       palette: { mode },
